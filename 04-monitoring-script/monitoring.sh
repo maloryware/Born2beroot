f1284b259ced8b14f4e5729458a1e797675649ad
Mal <malory@onenetbeyond.org>
2025-06-12 11:06:54 +0100

    added more layers of unparallelled insanity that only an open-source contributor driven by spite could possible conceive

--- 04-monitoring-script/monitoring.sh
+++ 04-monitoring-script/monitoring.sh
@@ -10,19 +10,7 @@
 # You wanted to one-line it? Here you go. Here's your disgusting, shitty excuse of a monitoring script. 
 # I hope you know you're literally bash Hitler for willingly choosing to use this.
 
-# wall << EOF
-cat << EOF
-	#	Architecture: $(uname -srm)
-	#	CPU Physical : $(cat /proc/cpuinfo | grep "physical id" | uniq | wc -l)
-	#	vCPU : $(cat /proc/cpuinfo | grep "processor" | uniq | wc -l)
-	#	Memory Usage: $(free -m | grep "Mem" | xargs echo | cut -d' ' -f3)/$(free -m | grep "Mem" | xargs echo | cut -d' ' -f2)MB ($(awk "BEGIN {printf \"%.2f\",$(free | grep "Mem" | xargs echo | cut -d' ' -f3)/$(free | grep "Mem" | xargs echo | cut -d' ' -f2)*100}")%)
-	#	Disk Usage: $(df -Bg | grep "^/dev" | grep -v "/boot$" | awk '{used += $3} END {print used}')/$(df -Bg | grep "^/dev" | grep -v "/boot$" | awk '{total += $2} END {print total}')GB ($(df -Bm | grep "^/dev" | grep -v "/boot$" | awk '{used += $3} {total += $2} END {printf("%d"), used / total * 100}')%)
-	#	CPU load: $(top -bn1 | grep Cpu | xargs echo | cut -d' ' -f2 | cut -d'%' -f1)%
-	#	Last boot: $(who -b | xargs echo | cut -d' ' -f3-)
-	#	LVM use: $(lsblk | grep lvm > /dev/null && echo yes || echo no)
-	#	Connections TCP : $(cat /proc/net/sockstat | grep "TCP" | cut -d' ' -f3)
-	#	User log: $(who | wc -l)
-	#	Network: IP $(hostname -I | xargs echo) ($(ip link show | grep ether | xargs echo | cut -d' ' -f2))
-	#	Sudo: $(cat /var/log/sudo/sudo.log | grep COMMAND | wc -l) commands
-	#	Note: I drink the blood of newborns for fun.
-EOF
+# 				 wall <<-EOF
+nl=$'\n';t=$'\t';cat <<-EOF
+	$t#$t Architecture: $(uname -srm)$nl$t#$t CPU Physical : $(cat /proc/cpuinfo | grep "physical id" | uniq | wc -l)$nl$t#$t vCPU : $(cat /proc/cpuinfo | grep "processor" | uniq | wc -l)$nl$t#$t Memory Usage: $(free -m | grep "Mem" | xargs echo | cut -d' ' -f3)/$(free -m | grep "Mem" | xargs echo | cut -d' ' -f2)MB ($(awk "BEGIN {printf \"%.2f\",$(free | grep "Mem" | xargs echo | cut -d' ' -f3)/$(free | grep "Mem" | xargs echo | cut -d' ' -f2)*100}")%)$nl$t#$t Disk Usage: $(df -Bg | grep "^/dev" | grep -v "/boot$" | awk '{used += $3} END {print used}')/$(df -Bg | grep "^/dev" | grep -v "/boot$" | awk '{total += $2} END {print total}')GB ($(df -Bm | grep "^/dev" | grep -v "/boot$" | awk '{used += $3} {total += $2} END {printf("%d"), used / total * 100}')%)$nl$t#$t CPU load: $(top -bn1 | grep Cpu | xargs echo | cut -d' ' -f2 | cut -d'%' -f1)%$nl$t#$t Last boot: $(who -b | xargs echo | cut -d' ' -f3-)$nl$t#$t LVM use: $(lsblk | grep lvm > /dev/null && echo yes || echo no)$nl$t#$t Connections TCP : $(cat /proc/net/sockstat | grep "TCP" | cut -d' ' -f3)$nl$t#$t User log: $(who | wc -l)$nl$t#$t Network: IP $(hostname -I | xargs echo) ($(ip link show | grep ether | xargs echo | cut -d' ' -f2))$nl$t#$t Sudo: $(cat /var/log/sudo/sudo.log | grep COMMAND | wc -l) commands$nl$t#$t Note: I drink the blood of newborns for fun.$nl
+	EOF
